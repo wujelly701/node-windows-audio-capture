291a29fcb3cd9395ba16405c9d5a041a44a7796c
wujelly701 <wujelly701@users.noreply.github.com>
2025-10-13 02:30:07 +0800

    feat: T025 捕获线程类声明 CaptureThread

---
+++ src/wasapi/capture_thread.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <thread>
+#include <atomic>
+#include <windows.h>
+#include "audio_client.h"
+
+class CaptureThread {
+public:
+    CaptureThread(AudioClient* client);
+    ~CaptureThread();
+
+    // 启动捕获线程
+    void Start();
+    // 停止捕获线程
+    void Stop();
+    // 查询是否正在运行
+    bool IsRunning() const;
+
+private:
+    void ThreadProc();
+    std::thread thread_;
+    std::atomic<bool> running_ = false;
+    AudioClient* client_;
+};

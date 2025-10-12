5b20905fa7adb39a13b6053567c918f4ef6b0833
wujelly701 <wujelly701@users.noreply.github.com>
2025-10-13 02:32:06 +0800

    feat: T031 N-API 包装类声明 AudioProcessor

---
+++ src/napi/audio_processor.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <napi.h>
+#include <memory>
+#include "wasapi/capture_thread.h"
+#include "wasapi/audio_client.h"
+
+class AudioProcessor : public Napi::ObjectWrap<AudioProcessor> {
+public:
+    static Napi::Object Init(Napi::Env env, Napi::Object exports);
+    AudioProcessor(const Napi::CallbackInfo& info);
+    ~AudioProcessor();
+
+private:
+    std::unique_ptr<AudioClient> client_;
+    std::unique_ptr<CaptureThread> thread_;
+    // N-API 方法声明（占位）
+    Napi::Value Start(const Napi::CallbackInfo& info);
+    Napi::Value Stop(const Napi::CallbackInfo& info);
+};
